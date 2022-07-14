753222892ac2d1ace483e96ae92cdc3c40912cea
Alina Costa <alinacosta@Alinas-MBP-2.lan>
2022-07-14 16:18:22 -0400

    create new js file

--- assets/js/script.js
+++
@@ -1,6 +0,0 @@
-// <!-- timer begins when START button is clicked -->
-// <!-- when a question is answered, the next question appears -->
-// <!-- remove 10 seconds from timer for each wrong asnwer -->
-// <!-- when timer runs out OR all questions are answered, quiz results appear -->
-// <!-- save initials and score -->
-// <!-- ability to view highscores -->
